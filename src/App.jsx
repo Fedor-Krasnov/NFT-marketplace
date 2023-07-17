adb741ba8f304f0bc4c676bf7cc7e8b9011825a7
Oleg Krasnov <oleg.v.krasnov@gmail.com>
2023-07-17 13:17:09 +0300

    fix(App): Подключение роутинга

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App.jsx';
 import './assets/styles/main.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<App />);
