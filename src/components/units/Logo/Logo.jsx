fea9ac8abd9b44cbbed83dbbd9c2e5b945ec34dd
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-07-18 01:05:21 +0300

    fix(Logo): Добавление стилевого файла

--- src/components/units/Logo/Logo.jsx
+++ src/components/units/Logo/Logo.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logoImage from './media/logo.svg';
+import './Logo.scss';
 
 const Logo = () => (
   <div className="logo">
     <Link to="/">
-      <img src={logoImage} className="main__logo" alt="logo" />
+      <img src={logoImage} alt="logo" />
     </Link>
-    <Link to="/" className="main__text__logo">
-      NFT Marketplace
-    </Link>
+    <Link to="/">NFT Marketplace</Link>
   </div>
 );
 
