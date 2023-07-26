887f94e970b92bc2061ee04f33203d70227c351c
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-07-27 00:49:32 +0300

    feat(Burger): Создание компонента

---
+++ src/components/Header/Burger/Burger.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Burger.scss';
+
+const Burger = () => (
+  <div className="burger">
+    <div className="burger__container">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+);
+
+export { Burger };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,24 +3,17 @@ import { Button } from '../units/Button';
 import { IconCode } from '../units/Icon';
 import { Logo } from '../units/Logo';
 import './Header.scss';
+import { Burger } from './Burger';
 import { HeaderMenu } from './HeaderMenu';
 
 const Header = () => (
   <header className="header">
     <Logo />
-    <div className="navbar">
+    <div className="header__navbar">
       <HeaderMenu />
-      <div className="header__sign-up">
-        <Button icon={IconCode.user} title="Sign Up" to="/pages/create-account.html" />
-      </div>
+      <Button icon={IconCode.user} title="Sign Up" to="/pages/create-account.html" />
     </div>
-    <div className="burger">
-      <div className="burger__container">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
+    <Burger />
   </header>
 );
 
