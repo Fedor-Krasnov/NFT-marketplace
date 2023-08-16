66c13be7da432a280d3cb74c7fa84a2950a6e775
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-08-17 01:08:10 +0300

    fix(Title): Корректировка отступов

--- src/components/units/Title/Title.jsx
+++ src/components/units/Title/Title.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Title.scss';
 
 const Title = ({ description, heading, title }) => (
-  <>
+  <div className="title">
     <h1 className={heading} dangerouslySetInnerHTML={{ __html: title }} />
     {description && <p dangerouslySetInnerHTML={{ __html: description }} />}
-  </>
+  </div>
 );
 
 export { Title };
