cb546fab6e6c1a265eb60f90f77e65e8306ba94c
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-09-03 00:32:10 +0300

    feat(Title): Добавление возможности показа кнопки

--- src/components/units/Title/Title.jsx
+++ src/components/units/Title/Title.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Title.scss';
+import { Button } from '../Button';
 
-const Title = ({ description, heading, title }) => (
+const Title = ({ buttonIcon, buttonTitle, description, heading, title }) => (
   <div className="title">
-    <h1 className={heading} dangerouslySetInnerHTML={{ __html: title }} />
-    {description && <p dangerouslySetInnerHTML={{ __html: description }} />}
+    <div>
+      <h1 className={heading} dangerouslySetInnerHTML={{ __html: title }} />
+      {description && <p dangerouslySetInnerHTML={{ __html: description }} />}
+    </div>
+    {buttonTitle && (
+      <div className="title__button">
+        <Button icon={buttonIcon} title={buttonTitle} />
+      </div>
+    )}
   </div>
 );
 
