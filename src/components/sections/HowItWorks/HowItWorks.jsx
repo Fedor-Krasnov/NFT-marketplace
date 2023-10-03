71d83e9c0cdcacb73cff68fc924084292e286bed
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-10-03 12:43:28 +0300

    feat(HowItWorks): Добавление Title в компонент HowItWorks

--- src/components/sections/HowItWorks/HowItWorks.jsx
+++ src/components/sections/HowItWorks/HowItWorks.jsx
@@ -3,6 +3,10 @@ import './HowItWorks.scss';
 import { howItWorksData } from '../../../mocks';
 import { Title } from '../../units/index.js';
 
-const HowItWorks = () => <section className="how-it-works"></section>;
+const HowItWorks = () => (
+  <section className="how-it-works">
+    <Title description={howItWorksData.description} heading="h2" title={howItWorksData.title} />
+  </section>
+);
 
 export { HowItWorks };
