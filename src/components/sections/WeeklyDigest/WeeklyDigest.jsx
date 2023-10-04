506b6595fe8d013e1862d23be439fdcc72d87d2e
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-10-04 12:27:18 +0300

    feat(WeeklyDigest): Добавление контента в компонент WeeklyDigest

--- src/components/sections/WeeklyDigest/WeeklyDigest.jsx
+++ src/components/sections/WeeklyDigest/WeeklyDigest.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './WeeklyDigest.scss';
-import { weeklyDigestData } from '../../../mocks/sections/weeklyDigest.js';
+import { weeklyDigestData } from '../../../mocks';
+import { Subscription } from '../../Subscription/index.js';
 import { Title } from '../../units/index.js';
 
 const WeeklyDigest = () => {
-  const { title, description, alt, src } = weeklyDigestData;
+  const { title, description, alt, src, image } = weeklyDigestData;
 
   return (
     <section className="weekly-digest">
-      <div className="weekly-digest__container"></div>
+      <div className="weekly-digest__container">
+        <div className="weekly-digest__img">
+          <img alt={image.alt} src={image.src} />
+        </div>
+        <div className="weekly-digest__information">
+          <Title description={description} title={title} />
+          <Subscription />
+        </div>
+      </div>
     </section>
   );
 };
--- src/mocks/sections/weeklyDigest.js
+++ src/mocks/sections/weeklyDigest.js
@@ -1,4 +1,8 @@
 export const weeklyDigestData = {
   title: 'Join our weekly digest',
   description: 'Get exclusive promotions & updates straight to your inbox.',
+  image: {
+    alt: 'NFT astronaut',
+    src: 'src/assets/media/images/nft-cards/nft-astronaut.jpg',
+  },
 };
