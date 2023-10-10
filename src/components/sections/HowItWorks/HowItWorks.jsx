885ac35aa29ef360676df7f16c6fd26b910eeeea
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-10-10 12:37:51 +0300

    feat(HowItWorks): Добавление контента, который приходит с backend

--- src/components/sections/HowItWorks/HowItWorks.jsx
+++ src/components/sections/HowItWorks/HowItWorks.jsx
@@ -3,39 +3,37 @@ import './HowItWorks.scss';
 import { howItWorksData } from '../../../mocks';
 import { Title } from '../../units/index.js';
 
-const HowItWorks = () => (
-  <section className="how-it-works">
-    <Title description={howItWorksData.description} heading="h2" title={howItWorksData.title} />
-    <div className="how-it-works__container">
-      <div className="explanatory-card">
-        <img alt="" src="../src-old/media/nft-cards/how-it-works/explanatory-card-1.png" />
-        <div>
-          <h3>Setup Your wallet</h3>
-          <p>
-            Set up&nbsp;your wallet of&nbsp;choice. Connect it&nbsp;to&nbsp;the Animarket by&nbsp;clicking the wallet
-            icon in&nbsp;the top right corner.
-          </p>
+const HowItWorks = () => {
+  const { offerCards, image } = howItWorksData;
+
+  return (
+    <section className="how-it-works">
+      <Title description={howItWorksData.description} heading="h2" title={howItWorksData.title} />
+      <div className="how-it-works__container">
+        <div className="explanatory-card">
+          {/* <img alt={image.alt} src={image.src} />*/}
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: offerCards.offer }} />
+            <p dangerouslySetInnerHTML={{ __html: offerCards.offerDescription }} />
+          </div>
+        </div>
+        <div className="explanatory-card">
+          {/* <img alt={image.alt} src={image.src} />*/}
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: offerCards.offer }} />
+            <p dangerouslySetInnerHTML={{ __html: offerCards.offerDescription }} />
+          </div>
+        </div>
+        <div className="explanatory-card">
+          {/* <img alt={image.alt} src={image.src} />*/}
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: offerCards.offer }} />
+            <p dangerouslySetInnerHTML={{ __html: offerCards.offerDescription }} />
+          </div>
         </div>
       </div>
-      <div className="explanatory-card">
-        <img alt="" src="../src-old/media/nft-cards/how-it-works/explanatory-card-2.png" />
-        <div>
-          <h3>Create Collection</h3>
-          <p>Upload your work and setup your collection. Add a&nbsp;description, social links and floor price.</p>
-        </div>
-      </div>
-      <div className="explanatory-card">
-        <img alt="" src="../src-old/media/nft-cards/how-it-works/explanatory-card-3.png" />
-        <div>
-          <h3>Start Earning</h3>
-          <p>
-            Choose between auctions and fixed-price listings. Start earning by&nbsp;selling your NFTs or&nbsp;trading
-            others.
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export { HowItWorks };
--- src/mocks/sections/howItWorksData.js
+++ src/mocks/sections/howItWorksData.js
@@ -1,4 +1,35 @@
 export const howItWorksData = {
   title: 'How it works',
   description: 'Find out how to get started',
+  offerCards: [
+    {
+      image: {
+        alt: 'Setup Your wallet',
+        src: '../src-old/media/nft-cards/how-it-works/explanatory-card-1.png',
+      },
+      offer: 'Setup Your wallet',
+      offerDescription:
+        'Set up&nbsp;your wallet of&nbsp;choice. Connect it&nbsp;to&nbsp;the Animarket by&nbsp;' +
+        'clicking the wallet icon in&nbsp;the top right corner.',
+    },
+    {
+      image: {
+        alt: 'Create Collection',
+        src: '../src-old/media/nft-cards/how-it-works/explanatory-card-2.png',
+      },
+      offer: 'Create Collection',
+      offerDescription:
+        'Upload your work and setup your collection. Add a&nbsp;description, social links and floor price.',
+    },
+    {
+      image: {
+        alt: 'Start Earning',
+        src: '../src-old/media/nft-cards/how-it-works/explanatory-card-3.png',
+      },
+      offer: 'Start Earning',
+      offerDescription:
+        'Choose between auctions and fixed-price listings. Start earning by&nbsp;selling your NFTs' +
+        ' or&nbsp;trading others.',
+    },
+  ],
 };
