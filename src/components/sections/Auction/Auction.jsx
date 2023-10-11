4b48f456c13a590cecd8f6a7772214d38866babd
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-10-11 12:47:43 +0300

    fix(Auction): Теперь контент приходит с backend

--- src/components/sections/Auction/Auction.jsx
+++ src/components/sections/Auction/Auction.jsx
@@ -1,53 +1,56 @@
 import React from 'react';
 import './Auction.scss';
+import { auctionData } from '../../../mocks/index.js';
+import { Button, IconCode } from '../../units/index.js';
 
-const Auction = () => (
-  <section className="auction">
-    <div className="auction__container">
-      <div className="auction__wrapper">
-        <div className="auction__author">
-          <div className="auction__avatar">
-            <img alt="avatar" src="../../src-old/media/users/user-7.svg" />
+const Auction = () => {
+  const { image, userName, nameNFT, title, countdownContent } = auctionData;
+
+  return (
+    <section className="auction">
+      <div className="auction__container">
+        <div className="auction__wrapper">
+          <div className="auction__author">
+            <div className="auction__avatar">
+              <img alt={image.alt} src={image.src} />
+            </div>
+            <div className="auction__user-name" dangerouslySetInnerHTML={{ __html: userName }} />
           </div>
-          <div className="auction__user-name">Shroomie</div>
-        </div>
-        <div className="auction__content">
-          <h2>Magic Mashrooms</h2>
-          <a className="button button_secondary" href="">
-            <img alt="" src="../../src-old/media/icons/see-all.svg" />
-            See NFT
-          </a>
-          <div className="countdown__container">
-            <div className="countdown">
-              <h4>Auction ends&nbsp;in:</h4>
-              <div className="countdown__content">
-                <div>
-                  <div className="countdown__value">59</div>
-                  <div>Hours</div>
-                </div>
-                <div>
-                  <div className="countdown__value">:</div>
-                  <div></div>
-                </div>
-                <div>
-                  <div className="countdown__value">59</div>
-                  <div>Minutes</div>
-                </div>
-                <div>
-                  <div className="countdown__value">:</div>
-                  <div></div>
-                </div>
-                <div>
-                  <div className="countdown__value">59</div>
-                  <div>Seconds</div>
+          <div className="auction__content">
+            <h2 dangerouslySetInnerHTML={{ __html: nameNFT }} />
+            <Button icon={IconCode.eye} secondary title={auctionData.buttonTitle} />
+            <div className="countdown__container">
+              <div className="countdown">
+                <h4 dangerouslySetInnerHTML={{ __html: title }} />
+                <div className="countdown__content">
+                  <div>
+                    <div className="countdown__value">59</div>
+                    <div dangerouslySetInnerHTML={{ __html: countdownContent.hours }} />
+                  </div>
+                  <div>
+                    <div className="countdown__value">:</div>
+                    <div></div>
+                  </div>
+                  <div>
+                    <div className="countdown__value">59</div>
+                    <div dangerouslySetInnerHTML={{ __html: countdownContent.minutes }} />
+                  </div>
+                  <div>
+                    <div className="countdown__value">:</div>
+                    <div></div>
+                  </div>
+                  <div>
+                    <div className="countdown__value">59</div>
+                    <div dangerouslySetInnerHTML={{ __html: countdownContent.seconds }} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export { Auction };
--- src/mocks/sections/auctionData.js
+++ src/mocks/sections/auctionData.js
@@ -1,1 +1,15 @@
-export const auctionData = {};
+export const auctionData = {
+  buttonTitle: 'See NFT',
+  title: 'Auction ends&nbsp;in:',
+  image: {
+    alt: 'avatar',
+    src: '../../src-old/media/users/user-7.svg',
+  },
+  userName: 'Shroomie',
+  nameNFT: 'Magic Mashrooms',
+  countdownContent: {
+    hours: 'Hours',
+    minutes: 'Minutes',
+    seconds: 'Seconds',
+  },
+};
