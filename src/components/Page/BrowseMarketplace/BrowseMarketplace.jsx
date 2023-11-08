7b679ad46e6caa765c34632c4c4c6ae8170fe766
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-11-08 11:44:30 +0300

    feat(BrowseMarketplace): Добавление стилей для начала страницы и перенос данных на backend

--- src/components/Page/BrowseMarketplace/BrowseMarketplace.jsx
+++ src/components/Page/BrowseMarketplace/BrowseMarketplace.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import './BrowseMarketplace.scss';
+import { browseMarketplace } from '../../../mocks/Page/browseMarketplace.js';
+import { Title } from '../../units/index.js';
 
-const BrowseMarketplace = () => (
-  <div className="browse-marketplace">
-    <div className="browse-marketplace__title">
-      <h1>Browse Marketplace</h1>
-      <p>Browse through more than 50k NFTs on&nbsp;the NFT Marketplace.</p>
-    </div>
-    <div className="browse-marketplace__search-string">
-      <label>
-        <input className="search" placeholder="Search your favourite NFTs" type="text" />
-      </label>
-      <div className="browse-marketplace__search-img">
-        <a href="">
-          <img alt="" className="magnifying-glass" src="../src-old/media/icons/magnifyingglass.svg" />
-        </a>
+const BrowseMarketplace = () => {
+  const { title, description, image, categoryOne, categoryOneSpan, categoryTwo, categoryTwoSpan } = browseMarketplace;
+
+  return (
+    <div className="browse-marketplace">
+      <div className="browse-marketplace__title">
+        <Title description={description} title={title} />
+      </div>
+      <div className="browse-marketplace__search-string">
+        <label>
+          <input className="search" placeholder="Search your favourite NFTs" type="text" />
+        </label>
+        <div className="browse-marketplace__search-img">
+          <a href="">
+            <img alt={image.alt} className="magnifying-glass" src={image.src} />
+          </a>
+        </div>
+      </div>
+      <div className="browse-marketplace__choice-point">
+        <div className="browse-marketplace__trait"></div>
+        <div className="browse-marketplace__buttons">
+          <a href="">
+            {categoryOne}
+            <span>{categoryOneSpan}</span>
+          </a>
+          <a href="">
+            {categoryTwo}
+            <span>{categoryTwoSpan}</span>
+          </a>
+        </div>
       </div>
     </div>
-    <div className="browse-marketplace__choice-point">
-      <div className="browse-marketplace__trait"></div>
-      <div className="browse-marketplace__buttons">
-        <a href="">
-          NFTs<span>302</span>
-        </a>
-        <a href="">
-          Collections<span>67</span>
-        </a>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export { BrowseMarketplace };
---
+++ src/mocks/Page/browseMarketplace.js
@@ -0,0 +1,12 @@
+export const browseMarketplace = {
+  title: 'Browse Marketplace',
+  description: 'Browse through more than 50k NFTs on&nbsp;the NFT Marketplace.',
+  image: {
+    alt: 'magnifying',
+    src: '../src-old/media/icons/magnifyingglass.svg',
+  },
+  categoryOne: 'NFTs',
+  categoryTwo: 'Collections',
+  categoryOneSpan: '302',
+  categoryTwoSpan: '67',
+};
