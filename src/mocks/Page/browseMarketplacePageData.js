9c05dac5770aa88b8af2951280c516fb42ff7c50
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-12-04 12:40:35 +0300

    feat(browseMarketplacePage): Добавление контента на страницу browseMarketplacePage

--- src/mocks/Page/browseMarketplacePageData.js
+++ src/mocks/Page/browseMarketplacePageData.js
@@ -13,12 +13,12 @@ export const browseMarketplacePageData = {
     {
       image: {
         alt: 'nft-card',
-        src: 'src/assets/media/images/nft-cards/7.jpg',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-1.jpg',
       },
       nftDetails: {
         title: 'Magic Mushroom 0325',
         image: {
-          src: '/src-old/media/users/user-7.svg',
+          src: '/src/assets/media/images/users/user-7.png',
         },
         username: 'Shroomie',
       },
@@ -30,14 +30,31 @@ export const browseMarketplacePageData = {
     {
       image: {
         alt: 'nft-card',
-        src: 'src/assets/media/images/nft-cards/7.jpg',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-2.jpg',
       },
       nftDetails: {
-        title: 'Magic Mushroom 0325',
+        title: 'Happy Robot 032',
         image: {
-          src: '/src-old/media/users/user-7.svg',
+          src: '/src/assets/media/images/users/user-12.png',
         },
-        username: 'Shroomie',
+        username: 'BeKind2Robots',
+      },
+      nftDetailedInformation: {
+        price: '9.43 ETH',
+        highestBid: '2.74 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-3.jpg',
+      },
+      nftDetails: {
+        title: 'Happy Robot 024',
+        image: {
+          src: '/src/assets/media/images/users/user-12.png',
+        },
+        username: 'BeKind2Robots',
       },
       nftDetailedInformation: {
         price: '1.63 ETH',
@@ -47,18 +64,154 @@ export const browseMarketplacePageData = {
     {
       image: {
         alt: 'nft-card',
-        src: 'src/assets/media/images/nft-cards/7.jpg',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-4.jpg',
       },
       nftDetails: {
-        title: 'Magic Mushroom 0325',
+        title: 'Designer Bear',
         image: {
-          src: '/src-old/media/users/user-7.svg',
+          src: '/src/assets/media/images/users/user-6.png',
         },
-        username: 'Shroomie',
+        username: 'Mr Fox',
       },
       nftDetailedInformation: {
-        price: '1.63 ETH',
-        highestBid: '0.33 wETH',
+        price: '6.51 ETH',
+        highestBid: '4.85 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-5.jpg',
+      },
+      nftDetails: {
+        title: 'Colorful Dog 0356',
+        image: {
+          src: '/src/assets/media/images/users/user-15.png',
+        },
+        username: 'Keepitreal',
+      },
+      nftDetailedInformation: {
+        price: '7.45 ETH',
+        highestBid: '2.67 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-6.jpg',
+      },
+      nftDetails: {
+        title: 'Dancing Robot 0312',
+        image: {
+          src: '/src/assets/media/images/users/user-8.png',
+        },
+        username: 'Robotica',
+      },
+      nftDetailedInformation: {
+        price: '2.56 ETH',
+        highestBid: '7.27 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-7.jpg',
+      },
+      nftDetails: {
+        title: 'Cherry Blossom Girl 035',
+        image: {
+          src: '/src/assets/media/images/users/user-14.png',
+        },
+        username: 'MoonDancer',
+      },
+      nftDetailedInformation: {
+        price: '3.62 ETH',
+        highestBid: '1.35 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-8.jpg',
+      },
+      nftDetails: {
+        title: 'Space Travel',
+        image: {
+          src: '/src/assets/media/images/users/user-15.png',
+        },
+        username: 'NebulaKid',
+      },
+      nftDetailedInformation: {
+        price: '5.23 ETH',
+        highestBid: '4.12 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-9.jpg',
+      },
+      nftDetails: {
+        title: 'Sunset Dimension',
+        image: {
+          src: '/src/assets/media/images/users/user-10.png',
+        },
+        username: 'Animakid',
+      },
+      nftDetailedInformation: {
+        price: '8.45 ETH',
+        highestBid: '45.23 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-10.jpg',
+      },
+      nftDetails: {
+        title: 'Desert Walk',
+        image: {
+          src: '/src/assets/media/images/users/user-7.png',
+        },
+        username: 'Catch 22',
+      },
+      nftDetailedInformation: {
+        price: '4.37 ETH',
+        highestBid: '23.56 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-11.jpg',
+      },
+      nftDetails: {
+        title: 'IceCream Ape 0324',
+        image: {
+          src: '/src/assets/media/images/users/user-4.png',
+        },
+        username: 'Ice Ape Club',
+      },
+      nftDetailedInformation: {
+        price: '45.3 ETH',
+        highestBid: '2.34 wETH',
+      },
+    },
+    {
+      image: {
+        alt: 'nft-card',
+        src: '/src/assets/media/images/nft-cards/nft-card-page-12.jpg',
+      },
+      nftDetails: {
+        title: 'Colorful Dog 0344',
+        image: {
+          src: '/src/assets/media/images/users/user-13.png',
+        },
+        username: 'PuppyPower',
+      },
+      nftDetailedInformation: {
+        price: '5.37 ETH',
+        highestBid: '7.98 wETH',
       },
     },
   ],
