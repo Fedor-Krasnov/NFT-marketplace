1e6dc499c01cd8aec1f8772d16f3ed267e1eaae5
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-12-06 11:40:08 +0300

    feat(UserProfilePageData): Добавление шаблона для старницы UserProfilePageData

---
+++ src/components/Page/UserProfilePage/UserProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UserProfilePage.scss';
+import { userProfilePageData } from '../../../mocks/Page/index.js';
+
+const UserProfilePage = () => {
+  const {} = userProfilePageData;
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export { UserProfilePage };
--- src/mocks/Page/index.js
+++ src/mocks/Page/index.js
@@ -1,3 +1,5 @@
 export { authorizationPageData } from './authorizationPageData.js';
 export { connectWalletPageData } from './connectWalletPageData.js';
 export { topCreatorsPageData } from './topCreatorsPageData.js';
+export { browseMarketplacePageData } from './browseMarketplacePageData.js';
+export { userProfilePageData } from './userProfilePageData.js';
