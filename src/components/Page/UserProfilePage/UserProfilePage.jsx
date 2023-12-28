be2fd53ca161c7a947ce01a42b77ba4d0bdb6423
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2023-12-28 05:20:37 +0300

    feat(UserProfilePage): Добавление работ пользователя на его страницу

--- src/components/Page/UserProfilePage/UserProfilePage.jsx
+++ src/components/Page/UserProfilePage/UserProfilePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './UserProfilePage.scss';
 import { UserProfileSection, DetailedUserProfileSection } from '../../sections';
+import { WorkLayoutSection } from '../../sections/WorkLayoutSection';
 
 const UserProfilePage = () => (
   <>
     <UserProfileSection />
     <DetailedUserProfileSection />
+    <WorkLayoutSection />
   </>
 );
 
