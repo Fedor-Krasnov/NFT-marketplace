0a9747104edc6ecd916372481e2228cb80eed3fd
Fedor Krasnov <fkrasnov66@gmail.com>
2024-01-04 00:57:02 +0300

    feat(TopCreatorsSection): Добавление ссылок на страницы пользователей

--- src/components/sections/TopCreatorsSection/TopCreatorsSection.jsx
+++ src/components/sections/TopCreatorsSection/TopCreatorsSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './TopCreatorsSection.scss';
+import { Link } from 'react-router-dom';
 import { topCreatorsPageData } from '../../../mocks/Page';
 
 const topCreatorsSectionClassName = 'top-creators-section';
@@ -15,16 +16,19 @@ const TopCreatorsSection = () => (
     </div>
     {topCreatorsPageData.userTop.map(({ image, userName, change, nftSold, volume }, userTopIndex) => (
       <div key={userName} className={`${topCreatorsSectionClassName}__creator`}>
-        <div className={`${topCreatorsSectionClassName}__serial-number`}>{userTopIndex + 1}</div>
-        <div className={`${topCreatorsSectionClassName}__artist-details`}>
-          <div className={`${topCreatorsSectionClassName}__avatar`}>
-            <img alt={image.alt} src={image.src} />
+        <div className={`${topCreatorsSectionClassName}__creator-container`}>
+          <div className={`${topCreatorsSectionClassName}__serial-number`}>{userTopIndex + 1}</div>
+          <div className={`${topCreatorsSectionClassName}__artist-details`}>
+            <div className={`${topCreatorsSectionClassName}__avatar`}>
+              <img alt={image.alt} src={image.src} />
+            </div>
+            <p className={`${topCreatorsSectionClassName}__name`} dangerouslySetInnerHTML={{ __html: userName }} />
           </div>
-          <p className={`${topCreatorsSectionClassName}__name`} dangerouslySetInnerHTML={{ __html: userName }} />
+          <div className={`${topCreatorsSectionClassName}__change`} dangerouslySetInnerHTML={{ __html: change }} />
+          <d className={`${topCreatorsSectionClassName}__nft-sold`} dangerouslySetInnerHTML={{ __html: nftSold }} />
+          <div className={`${topCreatorsSectionClassName}__volume`} dangerouslySetInnerHTML={{ __html: volume }} />
         </div>
-        <div className={`${topCreatorsSectionClassName}__change`} dangerouslySetInnerHTML={{ __html: change }} />
-        <d className={`${topCreatorsSectionClassName}__nft-sold`} dangerouslySetInnerHTML={{ __html: nftSold }} />
-        <div className={`${topCreatorsSectionClassName}__volume`} dangerouslySetInnerHTML={{ __html: volume }} />
+        <Link to={`/user-profile/${userName}`} />
       </div>
     ))}
   </section>
