6db22c4d8c0feb196efad56b8d273bff40015cd3
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-08 06:14:54 +0300

    refactor(NFTDetails): Рефакторинг классов

--- src/components/NFTDetails/NFTDetails.jsx
+++ src/components/NFTDetails/NFTDetails.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './NFTDetails.scss';
 
+const nftDetailsClassName = 'nft-details';
+
 const NFTDetails = ({ data }) => {
   const { image, title, username } = data;
 
   return (
-    <div className="nft-details">
+    <div className={nftDetailsClassName}>
       <h3 dangerouslySetInnerHTML={{ __html: title }} />
-      <div className="nft-details__user">
-        <div className="nft-details__avatar">{image && <img alt={image.alt} src={image.src} />}</div>
-        <div className="nft-details__username" dangerouslySetInnerHTML={{ __html: username }} />
+      <div className={`${nftDetailsClassName}__user`}>
+        <div className={`${nftDetailsClassName}__avatar`}>{image && <img alt={image.alt} src={image.src} />}</div>
+        <div className={`${nftDetailsClassName}__username`} dangerouslySetInnerHTML={{ __html: username }} />
       </div>
     </div>
   );
