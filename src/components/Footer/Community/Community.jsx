5889df063f50d19d4d313d9355c646b0f27108c1
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-08 05:18:59 +0300

    refactor(Footer/Community): Рефакторинг классов

--- src/components/Footer/Community/Community.jsx
+++ src/components/Footer/Community/Community.jsx
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 import { communityData } from '../../../mocks';
 import { Icon, IconCode } from '../../units';
 
+const communityClassName = 'community';
+
 const Community = ({ className }) => (
   <div className={classNames('community', className)}>
-    <ul className="community__contacts">
+    <ul className={`${communityClassName}__contacts`}>
       {communityData.map(({ link, network }) => (
         <li key={network}>
           <Link to={link}>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,22 +6,24 @@ import { Subscription } from '../Subscription';
 import { Logo } from '../units';
 import { Community } from './Community';
 
+const footerClassName = 'footer';
+
 const Footer = () => {
   const { copyright, descriptions, titles } = footerData;
 
   return (
-    <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__content">
-          <div className="footer__logo-and-community">
-            <Logo className="footer__logo" />
+    <footer className={footerClassName}>
+      <div className={`${footerClassName}__container`}>
+        <div className={`${footerClassName}__content`}>
+          <div className={`${footerClassName}__logo-and-community`}>
+            <Logo className={`${footerClassName}__logo`} />
             <p dangerouslySetInnerHTML={{ __html: descriptions.text1 }} />
             <p dangerouslySetInnerHTML={{ __html: descriptions.text2 }} />
-            <Community className="footer__community" />
+            <Community className={`${footerClassName}__community`} />
           </div>
           <div>
             <h4 dangerouslySetInnerHTML={{ __html: titles.explore }} />
-            <Menu className="footer__menu" isGridRows />
+            <Menu className={`${footerClassName}__menu`} isGridRows />
           </div>
           <div>
             <h4 dangerouslySetInnerHTML={{ __html: titles.joinOur }} />
@@ -29,7 +31,7 @@ const Footer = () => {
             <Subscription />
           </div>
         </div>
-        <div className="footer__copyright">
+        <div className={`${footerClassName}__copyright`}>
           <p dangerouslySetInnerHTML={{ __html: copyright }} />
         </div>
       </div>
