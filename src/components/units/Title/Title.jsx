7cc8089c10303934f0bae5f15c166f259544f1f7
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-09 06:48:21 +0300

    refactor(Title): Рефакторинг классов

--- src/components/units/Title/Title.jsx
+++ src/components/units/Title/Title.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './Title.scss';
 import { Button } from '../Button';
 
+const titleClassName = 'title';
+
 const Title = ({ buttonIcon, buttonTitle, description, heading, title, to }) => (
-  <div className="title">
+  <div className={titleClassName}>
     <div>
       <h1 className={heading} dangerouslySetInnerHTML={{ __html: title }} />
       {description && <p dangerouslySetInnerHTML={{ __html: description }} />}
     </div>
     {buttonTitle && (
-      <div className="title__button">
+      <div className={`${titleClassName}__button`}>
         <Button icon={buttonIcon} outline title={buttonTitle} to={to} />
       </div>
     )}
