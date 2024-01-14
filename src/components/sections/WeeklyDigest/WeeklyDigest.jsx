d37a61414a2c31b77291c20459c47eaa8690cc61
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-14 17:50:28 +0300

    refactor(WeeklyDigest): Рефактор классов

--- src/components/sections/WeeklyDigest/WeeklyDigest.jsx
+++ src/components/sections/WeeklyDigest/WeeklyDigest.jsx
@@ -4,16 +4,18 @@ import { weeklyDigestData } from '../../../mocks';
 import { Subscription } from '../../Subscription';
 import { Title } from '../../units';
 
+const weeklyDigestClassName = 'weekly-digest';
+
 const WeeklyDigest = () => {
   const { title, description, image } = weeklyDigestData;
 
   return (
-    <section className="weekly-digest">
-      <div className="weekly-digest__container">
-        <div className="weekly-digest__img">
+    <section className={weeklyDigestClassName}>
+      <div className={`${weeklyDigestClassName}__container`}>
+        <div className={`${weeklyDigestClassName}__img`}>
           <img alt={image.alt} src={image.src} />
         </div>
-        <div className="weekly-digest__information">
+        <div className={`${weeklyDigestClassName}__information`}>
           <Title description={description} heading="h2" title={title} />
           <Subscription />
         </div>
