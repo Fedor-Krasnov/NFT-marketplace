2e460a603d73cd64a1c1ce877ec55f789637cd14
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-30 22:29:52 +0300

    feat(NFTCard/NFTDetails): Добавление функционала ссылок, в карточки, для прехода в профиль автора

--- src/components/NFTDetails/NFTDetails.jsx
+++ src/components/NFTDetails/NFTDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NFTDetails.scss';
+import { Link } from 'react-router-dom';
 
 const nftDetailsClassName = 'nft-details';
 
@@ -9,10 +10,12 @@ const NFTDetails = ({ data }) => {
   return (
     <div className={nftDetailsClassName}>
       <h3 dangerouslySetInnerHTML={{ __html: title }} />
-      <div className={`${nftDetailsClassName}__user`}>
-        <div className={`${nftDetailsClassName}__avatar`}>{image && <img alt={image.alt} src={image.src} />}</div>
-        <div className={`${nftDetailsClassName}__username`} dangerouslySetInnerHTML={{ __html: username }} />
-      </div>
+      <Link to="tmp">
+        <div className={`${nftDetailsClassName}__user`}>
+          <div className={`${nftDetailsClassName}__avatar`}>{image && <img alt={image.alt} src={image.src} />}</div>
+          <div className={`${nftDetailsClassName}__username`} dangerouslySetInnerHTML={{ __html: username }} />
+        </div>
+      </Link>
     </div>
   );
 };
