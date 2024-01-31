b5059112edb49aa8fae86eae60035b8c0fa9be73
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-01-31 10:05:03 +0300

    feat(TopCreators/UserCreators): Добавление функционала, который позволяет переходть в профиль автора, со списка top-creators

--- src/components/sections/TopCreators/UserCreators/UserCreators.jsx
+++ src/components/sections/TopCreators/UserCreators/UserCreators.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './UserCreators.scss';
+import { Link } from 'react-router-dom';
 import { topCreatorsData } from '../../../../mocks';
 
 const userCreatorsClassName = 'creators';
@@ -7,18 +8,20 @@ const userCreatorsClassName = 'creators';
 const UserCreators = () => (
   <div className={userCreatorsClassName}>
     {topCreatorsData.userCreators.map(({ image, userName, totalSales }, userCreatorIndex) => (
-      <div key={userName} className={`${userCreatorsClassName}__user`}>
-        <span className={`${userCreatorsClassName}__number`}>{userCreatorIndex + 1}</span>
-        <div className={`${userCreatorsClassName}__avatar`}>
-          <img alt={image.alt} src={image.src} />
+      <Link to="tmp">
+        <div key={userName} className={`${userCreatorsClassName}__user`}>
+          <span className={`${userCreatorsClassName}__number`}>{userCreatorIndex + 1}</span>
+          <div className={`${userCreatorsClassName}__avatar`}>
+            <img alt={image.alt} src={image.src} />
+          </div>
+          <div className={`${userCreatorsClassName}__details`}>
+            <h3>{userName}</h3>
+            <p>
+              <span>Total Sales:</span> {totalSales}
+            </p>
+          </div>
         </div>
-        <div className={`${userCreatorsClassName}__details`}>
-          <h3>{userName}</h3>
-          <p>
-            <span>Total Sales:</span> {totalSales}
-          </p>
-        </div>
-      </div>
+      </Link>
     ))}
   </div>
 );
