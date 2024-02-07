259b6b831420954f391edaddc48af6efb37874e8
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-02-07 12:21:07 +0300

    feat(DescriptionNFTSection/NFTSection/descriptionNFTSectionData): Добавление заготовки для секции DescriptionNFTSection

--- src/components/Page/PageAuctionNFTPage/DescriptionNFTSection/DescriptionNFTSection.jsx
+++ src/components/Page/PageAuctionNFTPage/DescriptionNFTSection/DescriptionNFTSection.jsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import './DescriptionNFTSection.scss';
+import { Link } from 'react-router-dom';
+import { descriptionNFTSectionData } from '../../../../mocks/Page';
+import { Countdown } from '../../../sections/Auction/Countdown/index.js';
+import { Icon, IconCode, Title } from '../../../units';
 
-const DescriptionNFTSection = () => <div></div>;
+const DescriptionNFTSectionClassName = 'description-nft-section';
+
+const DescriptionNFTSection = () => {
+  const { title, description, userName, image, username } = descriptionNFTSectionData;
+
+  return (
+    <section className={DescriptionNFTSectionClassName}>
+      <div className={`${DescriptionNFTSectionClassName}__main-information`}>
+        <div className={`${DescriptionNFTSectionClassName}__description-details`}>
+          <Title description={description} title={title} />
+          <div className={`${DescriptionNFTSectionClassName}__author`}>
+            <span>Created By</span>
+            <Link to={`/user-profile/${userName}`}>
+              <div className={`${DescriptionNFTSectionClassName}__user`}>
+                <div className={`${DescriptionNFTSectionClassName}__avatar`}>
+                  {image && <img alt={image.alt} src={image.src} />}
+                </div>
+                <div
+                  className={`${DescriptionNFTSectionClassName}__username`}
+                  dangerouslySetInnerHTML={{ __html: username }}
+                />
+              </div>
+            </Link>
+          </div>
+          <div className={`${DescriptionNFTSectionClassName}__description`}>
+            <span>Description</span>
+          </div>
+          <div className={`${DescriptionNFTSectionClassName}__details`}>
+            <span>Details</span>
+          </div>
+          <div className={`${DescriptionNFTSectionClassName}__tags`}>
+            <span>Tags</span>
+          </div>
+        </div>
+      </div>
+      <div className={`${DescriptionNFTSectionClassName}__countdown`}>
+        <Countdown />
+      </div>
+    </section>
+  );
+};
 
 export { DescriptionNFTSection };
---
+++ src/mocks/Page/descriptionNFTSectionData.js
@@ -0,0 +1,9 @@
+export const descriptionNFTSectionData = {
+  title: 'The Orbitians',
+  description: 'Minted on Sep 30, 2022',
+  image: {
+    alt: 'user',
+    src: '/src/assets/media/images/users/user-9.png',
+  },
+  username: 'Orbitian',
+};
