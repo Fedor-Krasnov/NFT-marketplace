0b44b0dd702674127cdccc77ae996eab58fa37b7
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-02-07 10:10:11 +0300

    fix(UserProfileSection): Исправление кода картинки

--- src/components/sections/UserProfileSection/UserProfileSection.jsx
+++ src/components/sections/UserProfileSection/UserProfileSection.jsx
@@ -8,7 +8,7 @@ const UserProfileSection = () => (
     <div className={`${userProfileSectionClassName}__background`} />
     <div className={`${userProfileSectionClassName}__avatar-container`}>
       <div className={`${userProfileSectionClassName}__avatar`}>
-        <img alt="" src="../src/assets/media/images/users/square/user-10.png" />
+        <img alt="" src="/src/assets/media/images/users/square/user-10.png" />
       </div>
     </div>
   </section>
