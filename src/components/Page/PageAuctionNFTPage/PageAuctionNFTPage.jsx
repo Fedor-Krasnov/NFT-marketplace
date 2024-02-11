82a86ba67b5ad12ec2f83d011b9bf542ffc67e92
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-02-11 05:45:35 +0300

    feat(PageAuctionNFTPage): Добавление адаптивной версии

--- src/components/Page/PageAuctionNFTPage/PageAuctionNFTPage.jsx
+++ src/components/Page/PageAuctionNFTPage/PageAuctionNFTPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import './PageAuctionNFTPage.scss';
 import { useParams } from 'react-router-dom';
+import { pageAuctionNFTPageData } from '../../../mocks/Page';
 import { WorkLayoutSection } from '../../sections';
-import { DescriptionNFTSectionPage } from './DescriptionNFTSectionPage/index.js';
-import { NFTSectionPage } from './NFTSectionPage/index.js';
+import { Button, IconCode } from '../../units';
+import { DescriptionNFTSectionPage } from './DescriptionNFTSectionPage';
+import { NFTSectionPage } from './NFTSectionPage';
+
+const PageAuctionNftPageClassName = 'nft-auction-page';
 
 const PageAuctionNftPage = () => {
+  const { titleLink, buttonTitle } = pageAuctionNFTPageData;
   const { userName } = useParams();
 
   return (
@@ -12,6 +18,15 @@ const PageAuctionNftPage = () => {
       <NFTSectionPage />
       <DescriptionNFTSectionPage />
       <WorkLayoutSection userName={userName} />
+      <Button
+        buttonTitle={buttonTitle}
+        className={`${PageAuctionNftPageClassName}__button`}
+        icon={IconCode.arrowToTheRight}
+        outline
+        title={titleLink}
+        to={`/user-profile/${userName}`}
+        width="fill"
+      />
     </>
   );
 };
