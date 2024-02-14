10fad5d0dde31116a3fd10ad5d56e56ab73c5ae9
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-02-14 10:23:55 +0300

    feat(communityData): Добавление ссылок в community

--- src/mocks/communityData.js
+++ src/mocks/communityData.js
@@ -1,5 +1,8 @@
 export const communityData = [
-  { link: 'link', network: 'discord' },
-  { link: 'link', network: 'youtube' },
-  { link: 'link', network: 'instagram' },
+  { link: 'https://discord.com/', network: 'discord' },
+  { link: 'https://www.youtube.com/channel/UC3Q7g8MvNPqKMiVj5ltsYfQ', network: 'youtube' },
+  {
+    link: 'https://the-flow.ru/uploads/images/resize/830x0/adaptiveResize/16/09/76/94/57/7b7e07f40cd7.jpg',
+    network: 'instagram',
+  },
 ];
