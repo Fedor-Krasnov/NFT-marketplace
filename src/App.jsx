2e8d0e01dff44112347611dd9e29fbe8c3b6d0e7
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-02-14 12:25:52 +0300

    feat(App): Добавление скрола к шапке страницы, при переключении между ними

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Page } from './components/Page';
 import { AuthorizationPage } from './components/Page/AuthorizationPage';
 import { BrowseMarketplacePage } from './components/Page/BrowseMarketplacePage';
@@ -9,18 +9,26 @@ import { PageAuctionNftPage } from './components/Page/PageAuctionNFTPage';
 import { TopCreatorsPage } from './components/Page/TopCreatorsPage';
 import { UserProfilePage } from './components/Page/UserProfilePage';
 
-const App = () => (
-  <Page>
-    <Routes>
-      <Route element={<HomePage />} path="/" />
-      <Route element={<AuthorizationPage />} path="/authorization" />
-      <Route element={<ConnectWalletPage />} path="/connect-wallet" />
-      <Route element={<TopCreatorsPage />} path="/top-creators" />
-      <Route element={<BrowseMarketplacePage />} path="/browse-marketplace" />
-      <Route element={<UserProfilePage />} path="/user-profile/:userName" />
-      <Route element={<PageAuctionNftPage />} path="/page-Auction-nft-Page" />
-    </Routes>
-  </Page>
-);
+const App = () => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <Page>
+      <Routes>
+        <Route element={<HomePage />} path="/" />
+        <Route element={<AuthorizationPage />} path="/authorization" />
+        <Route element={<ConnectWalletPage />} path="/connect-wallet" />
+        <Route element={<TopCreatorsPage />} path="/top-creators" />
+        <Route element={<BrowseMarketplacePage />} path="/browse-marketplace" />
+        <Route element={<UserProfilePage />} path="/user-profile/:userName" />
+        <Route element={<PageAuctionNftPage />} path="/page-Auction-nft-Page" />
+      </Routes>
+    </Page>
+  );
+};
 
 export { App };
