0c1b7592b02ff255345264d49a291776a9227825
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-08 23:51:21 +0300

    refactor(Page): Удаление лишнего кода

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
-import pageStyles from './Page.scss';
-
-const pageClassName = 'page';
 
 const Page = ({ children }) => (
-  <div className={pageStyles[pageClassName]}>
+  <>
     <Header />
     <main>{children}</main>
     <Footer />
-  </div>
+  </>
 );
 
 export { Page };
