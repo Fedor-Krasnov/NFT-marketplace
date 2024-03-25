b900dfa813c7ec9b49724c92a45d9a008598e2c9
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-25 14:02:20 +0300

    build(vite.config): Добавление конфига, для переноса сайта на модульный CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import crypto from 'crypto';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: (name) => {
+        const hash = crypto.createHash('md5').digest('base64').substring(0, 5);
+
+        return `${name}__${hash}`;
+      },
+    },
+  },
+});
