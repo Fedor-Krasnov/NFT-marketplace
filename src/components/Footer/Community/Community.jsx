67976898e98bce17425885fbcd4bf2815634fa63
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-25 14:08:59 +0300

    chore(Footer/Community): CSS-module

--- src/components/Footer/Community/Community.jsx
+++ src/components/Footer/Community/Community.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
-import './Community.scss';
 import { Link } from 'react-router-dom';
 import { communityData } from '../../../mocks';
 import { Icon, IconCode } from '../../units';
+import communityStyles from './Community.module.scss';
 
 const communityClassName = 'community';
 
 const Community = ({ className }) => (
-  <div className={classNames('community', className)}>
-    <ul className={`${communityClassName}__contacts`}>
+  <div className={classNames(communityStyles['community'], className)}>
+    <ul className={communityStyles[`${communityClassName}__contacts`]}>
       {communityData.map(({ link, network }) => (
         <li key={network}>
           <Link to={link}>
