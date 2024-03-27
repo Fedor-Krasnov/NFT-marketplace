ad4486c5ad105a27e8bfc8722129ebc68ac68be7
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-27 13:06:39 +0300

    feat(AuthorizationPage): CSS-module

--- src/components/Page/AuthorizationPage/AuthorizationPage.jsx
+++ src/components/Page/AuthorizationPage/AuthorizationPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './AuthorizationPage.scss';
 import { authorizationPageData } from '../../../mocks/Page';
 import { Button, Title } from '../../units';
 import { Input } from '../../units/Input';
+import authorizationPageStyles from './AuthorizationPage.module.scss';
 
 const authorizationPageClassName = 'create-account-main';
 
@@ -10,15 +10,15 @@ const AuthorizationPage = () => {
   const { description, title, entryField, titleButton } = authorizationPageData;
 
   return (
-    <div className={authorizationPageClassName}>
-      <div className={`${authorizationPageClassName}__img-container`}>
-        <div className={`${authorizationPageClassName}__img`}></div>
+    <div className={authorizationPageStyles[authorizationPageClassName]}>
+      <div className={authorizationPageStyles[`${authorizationPageClassName}__img-container`]}>
+        <div className={authorizationPageStyles[`${authorizationPageClassName}__img`]}></div>
       </div>
-      <div className={`${authorizationPageClassName}__registration`}>
-        <div className={`${authorizationPageClassName}__title`}>
+      <div className={authorizationPageStyles[`${authorizationPageClassName}__registration`]}>
+        <div className={authorizationPageStyles[`${authorizationPageClassName}__title`]}>
           <Title description={description} heading="h1" title={title} />
         </div>
-        <div className={`${authorizationPageClassName}__entry-field`}>
+        <div className={authorizationPageStyles[`${authorizationPageClassName}__entry-field`]}>
           {entryField.map((data) => (
             <Input key={data.id} data={data} />
           ))}
