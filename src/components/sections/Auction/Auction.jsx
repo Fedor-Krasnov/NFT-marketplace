f72a7607adafc67431e88ab0ff1aa5d9275f6700
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-30 10:40:45 +0300

    chore(Auction): CSS-module

--- src/components/sections/Auction/Auction.jsx
+++ src/components/sections/Auction/Auction.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Auction.scss';
 import { Link } from 'react-router-dom';
 import { auctionData } from '../../../mocks';
 import { Button, IconCode } from '../../units';
+import auctionStyles from './Auction.module.scss';
 import { Countdown } from './Countdown';
 
 const auctionClassName = 'auction';
@@ -11,18 +11,21 @@ const Auction = () => {
   const { image, userName, nameNFT } = auctionData;
 
   return (
-    <section className={auctionClassName}>
-      <div className={`${auctionClassName}__container`}>
-        <div className={`${auctionClassName}__wrapper`}>
+    <section className={auctionStyles[auctionClassName]}>
+      <div className={auctionStyles[`${auctionClassName}__container`]}>
+        <div className={auctionStyles[`${auctionClassName}__wrapper`]}>
           <Link to={`/user-profile/${userName}`}>
-            <div className={`${auctionClassName}__author`}>
-              <div className={`${auctionClassName}__avatar`}>
+            <div className={auctionStyles[`${auctionClassName}__author`]}>
+              <div className={auctionStyles[`${auctionClassName}__avatar`]}>
                 <img alt={image.alt} src={image.src} />
               </div>
-              <div className={`${auctionClassName}__user-name`} dangerouslySetInnerHTML={{ __html: userName }} />
+              <div
+                className={auctionStyles[`${auctionClassName}__user-name`]}
+                dangerouslySetInnerHTML={{ __html: userName }}
+              />
             </div>
           </Link>
-          <div className={`${auctionClassName}__content`}>
+          <div className={auctionStyles[`${auctionClassName}__content`]}>
             <h2 dangerouslySetInnerHTML={{ __html: nameNFT }} />
             <Button button icon={IconCode.eye} secondary title={auctionData.buttonTitle} to={auctionData.buttonLink} />
             <Countdown />
