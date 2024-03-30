536334b3eee0784b42f3cd461600fbb458b042b3
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-03-30 11:20:26 +0300

    chore(DetailedUserProfileSection): CSS-module

--- src/components/sections/DetailedUserProfileSection/DetailedUserProfileSection.jsx
+++ src/components/sections/DetailedUserProfileSection/DetailedUserProfileSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './DetailedUserProfileSection.scss';
 import { userProfilePageData } from '../../../mocks/Page';
 import { Community } from '../../Footer/Community';
 import { Button, IconCode, Statistics } from '../../units';
+import detailedUserProfileSectionStyles from './DetailedUserProfileSection.module.scss';
 
 const detailedUserProfileSectionClassName = 'detailed-user-profile-section';
 
@@ -10,46 +10,57 @@ const DetailedUserProfileSection = () => {
   const { statistics, userName, userBio } = userProfilePageData;
 
   return (
-    <div className={detailedUserProfileSectionClassName}>
-      <div className={`${detailedUserProfileSectionClassName}__main-information`}>
-        <div className={`${detailedUserProfileSectionClassName}__user-information`}>
-          <div className={`${detailedUserProfileSectionClassName}__user-main-information`}>
+    <div className={detailedUserProfileSectionStyles[detailedUserProfileSectionClassName]}>
+      <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__main-information`]}>
+        <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-information`]}>
+          <div
+            className={
+              detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-main-information`]
+            }
+          >
             <h1
-              className={`${detailedUserProfileSectionClassName}__user-name`}
+              className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-name`]}
               dangerouslySetInnerHTML={{ __html: userName }}
             />
-            <div className={`${detailedUserProfileSectionClassName}__button`}>
+            <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__button`]}>
               <Button icon={IconCode.copy} title="0xc03...B79C" />
               <Button
-                className={`${detailedUserProfileSectionClassName}__button-purple`}
+                className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__button-purple`]}
                 icon={IconCode.plus}
                 outline
                 title="Follow"
               />
             </div>
           </div>
-          <div className={`${detailedUserProfileSectionClassName}__user-description`}>
-            <div className={`${detailedUserProfileSectionClassName}__user-statistic`}>
-              <Statistics className={`${detailedUserProfileSectionClassName}__statistic`} statistics={statistics} />
+          <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-description`]}>
+            <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-statistic`]}>
+              <Statistics
+                className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__statistic`]}
+                statistics={statistics}
+              />
             </div>
             <div>
-              <div className={`${detailedUserProfileSectionClassName}__bio`}>Bio</div>
+              <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__bio`]}>Bio</div>
               <p
-                className={`${detailedUserProfileSectionClassName}__user-bio`}
+                className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-bio`]}
                 dangerouslySetInnerHTML={{ __html: userBio }}
               />
             </div>
-            <div className={`${detailedUserProfileSectionClassName}__links`}>
-              <div className={`${detailedUserProfileSectionClassName}__user-links`}>Links</div>
-              <Community className={`${detailedUserProfileSectionClassName}__community`} />
+            <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__links`]}>
+              <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__user-links`]}>
+                Links
+              </div>
+              <Community
+                className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__community`]}
+              />
             </div>
           </div>
         </div>
       </div>
       <div>
-        <div className={`${detailedUserProfileSectionClassName}__choice-point`}>
-          <div className={`${detailedUserProfileSectionClassName}__trait`}></div>
-          <div className={`${detailedUserProfileSectionClassName}__buttons`}>
+        <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__choice-point`]}>
+          <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__trait`]}></div>
+          <div className={detailedUserProfileSectionStyles[`${detailedUserProfileSectionClassName}__buttons`]}>
             <a href="">
               Created<span>302</span>
             </a>
