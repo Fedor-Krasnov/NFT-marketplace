e28818bccbbd0ccb3097995269594481816e44db
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-04-01 13:30:03 +0300

    chore(UserCreators): CSS-module

--- src/components/sections/TopCreators/UserCreators/UserCreators.jsx
+++ src/components/sections/TopCreators/UserCreators/UserCreators.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import './UserCreators.scss';
 import { Link } from 'react-router-dom';
 import { topCreatorsData } from '../../../../mocks';
+import userCreatorsStyles from './UserCreators.module.scss';
 
 const userCreatorsClassName = 'creators';
 
 const UserCreators = () => (
-  <div className={userCreatorsClassName}>
+  <div className={userCreatorsStyles[userCreatorsClassName]}>
     {topCreatorsData.userCreators.map(({ image, userName, totalSales }, userCreatorIndex) => (
       <Link to={`/user-profile/${userName}`}>
-        <div key={userName} className={`${userCreatorsClassName}__user`}>
-          <span className={`${userCreatorsClassName}__number`}>{userCreatorIndex + 1}</span>
-          <div className={`${userCreatorsClassName}__avatar`}>
+        <div key={userName} className={userCreatorsStyles[`${userCreatorsClassName}__user`]}>
+          <span className={userCreatorsStyles[`${userCreatorsClassName}__number`]}>{userCreatorIndex + 1}</span>
+          <div className={userCreatorsStyles[`${userCreatorsClassName}__avatar`]}>
             <img alt={image.alt} src={image.src} />
           </div>
-          <div className={`${userCreatorsClassName}__details`}>
+          <div className={userCreatorsStyles[`${userCreatorsClassName}__details`]}>
             <h3>{userName}</h3>
             <p>
               <span>Total Sales:</span> {totalSales}
