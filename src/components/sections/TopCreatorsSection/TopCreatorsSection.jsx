656f29aa5ab724bb16cc2d4b1bbbeea462b360d6
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-04-03 13:50:11 +0300

    chore(TopCreatorsSection): CSS-module

--- src/components/sections/TopCreatorsSection/TopCreatorsSection.jsx
+++ src/components/sections/TopCreatorsSection/TopCreatorsSection.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
-import './TopCreatorsSection.scss';
 import { Link } from 'react-router-dom';
 import { topCreatorsPageData } from '../../../mocks/Page';
+import topCreatorsSectionStyles from './TopCreatorsSection.module.scss';
 
 const topCreatorsSectionClassName = 'top-creators-section';
 
 const TopCreatorsSection = () => (
-  <section className={topCreatorsSectionClassName}>
-    <div className={`${topCreatorsSectionClassName}__list-header`}>
-      <p className={`${topCreatorsSectionClassName}__example-1`}>#</p>
-      <p className={`${topCreatorsSectionClassName}__example-2`}>Artist</p>
-      <p className={`${topCreatorsSectionClassName}__example-3`}>Change</p>
-      <p className={`${topCreatorsSectionClassName}__example-4`}>NFTs Sold</p>
-      <p className={`${topCreatorsSectionClassName}__example-5`}>Volume</p>
+  <section className={topCreatorsSectionStyles[topCreatorsSectionClassName]}>
+    <div className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__list-header`]}>
+      <p className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__example-1`]}>#</p>
+      <p className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__example-2`]}>Artist</p>
+      <p className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__example-3`]}>Change</p>
+      <p className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__example-4`]}>NFTs Sold</p>
+      <p className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__example-5`]}>Volume</p>
     </div>
     {topCreatorsPageData.userTop.map(({ image, userName, change, nftSold, volume }, userTopIndex) => (
-      <div key={userName} className={`${topCreatorsSectionClassName}__creator`}>
-        <div className={`${topCreatorsSectionClassName}__creator-container`}>
-          <div className={`${topCreatorsSectionClassName}__serial-number`}>{userTopIndex + 1}</div>
-          <div className={`${topCreatorsSectionClassName}__artist-details`}>
-            <div className={`${topCreatorsSectionClassName}__avatar`}>
+      <div key={userName} className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__creator`]}>
+        <div className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__creator-container`]}>
+          <div className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__serial-number`]}>
+            {userTopIndex + 1}
+          </div>
+          <div className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__artist-details`]}>
+            <div className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__avatar`]}>
               <img alt={image.alt} src={image.src} />
             </div>
-            <p className={`${topCreatorsSectionClassName}__name`} dangerouslySetInnerHTML={{ __html: userName }} />
+            <p
+              className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__name`]}
+              dangerouslySetInnerHTML={{ __html: userName }}
+            />
           </div>
-          <div className={`${topCreatorsSectionClassName}__change`} dangerouslySetInnerHTML={{ __html: change }} />
-          <d className={`${topCreatorsSectionClassName}__nft-sold`} dangerouslySetInnerHTML={{ __html: nftSold }} />
-          <div className={`${topCreatorsSectionClassName}__volume`} dangerouslySetInnerHTML={{ __html: volume }} />
+          <div
+            className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__change`]}
+            dangerouslySetInnerHTML={{ __html: change }}
+          />
+          <d
+            className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__nft-sold`]}
+            dangerouslySetInnerHTML={{ __html: nftSold }}
+          />
+          <div
+            className={topCreatorsSectionStyles[`${topCreatorsSectionClassName}__volume`]}
+            dangerouslySetInnerHTML={{ __html: volume }}
+          />
         </div>
         <Link to={`/user-profile/${userName}`} />
       </div>
