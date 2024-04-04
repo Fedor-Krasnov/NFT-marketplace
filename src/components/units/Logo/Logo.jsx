3d4151547f7ae7268557ecd1c5f49618a5852b52
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-04-05 02:04:41 +0300

    chore(Logo): CSS-module

--- src/components/units/Logo/Logo.jsx
+++ src/components/units/Logo/Logo.jsx
@@ -3,23 +3,25 @@ import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { pageCheck } from '../../../helpers';
 import { Icon, IconCode } from '../Icon';
-import './Logo.scss';
+import logoStyles from './Logo.module.scss';
+
+const logoClassName = 'logo';
 
 const Logo = ({ className, withoutText }) => {
   const { isMainPage } = pageCheck();
-  const logoClassName = classNames('logo', className);
+  const logoClassNames = classNames(logoStyles[logoClassName], className);
 
   const logo = (
     <>
-      <Icon className="logo__icon" code={IconCode.logo} />
+      <Icon className={logoStyles[`${logoClassName}__icon`]} code={IconCode.logo} />
       {!withoutText && <span>NFT Marketplace</span>}
     </>
   );
 
   return isMainPage ? (
-    <div className={logoClassName}>{logo}</div>
+    <div className={logoClassNames}>{logo}</div>
   ) : (
-    <Link className={logoClassName} to="/">
+    <Link className={logoClassNames} to="/">
       {logo}
     </Link>
   );
--- src/components/units/index.js
+++ src/components/units/index.js
@@ -1,5 +1,6 @@
 export { Button } from './Button';
 export { Icon, IconCode } from './Icon';
+export { Input } from './Input';
 export { Logo } from './Logo';
 export { Statistics } from './Statistics';
 export { Text } from './Text';
