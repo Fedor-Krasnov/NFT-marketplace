9c2a2e4008b2752be4452dde6f8a9e3ce1a32c63
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-04-04 12:42:08 +0300

    “button-fix-error”

--- src/components/units/Button/Button.jsx
+++ src/components/units/Button/Button.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Button.scss';
+import buttonStyles from './Button.module.scss';
 import classNames from 'classnames';
 import { Icon } from '../Icon';
 
+const buttonClassName = 'button';
+
 const Button = ({ className, icon, outline, secondary, title, to, width }) => {
   const buttonClassNames = classNames(
-    'button',
-    { ['button_icon']: icon },
-    { ['button_outline']: outline },
-    { ['button_secondary']: secondary },
-    { [`button_width-${width}`]: width },
+    buttonStyles[buttonClassName],
+    { [buttonStyles[`${buttonClassName}_icon`]]: icon },
+    { [buttonStyles[`${buttonClassName}_outline`]]: outline },
+    { [buttonStyles[`${buttonClassName}_secondary`]]: secondary },
+    { [buttonStyles[`${buttonClassName}_width-${width}`]]: width },
     className,
   );
 
@@ -22,11 +24,11 @@ const Button = ({ className, icon, outline, secondary, title, to, width }) => {
   );
 
   return to ? (
-    <Link to={to} className={buttonClassNames}>
+    <Link to={to} className={buttonStyles[buttonClassNames]}>
       {buttonTitle}
     </Link>
   ) : (
-    <button className={buttonClassNames}>{buttonTitle}</button>
+    <button className={buttonStyles[buttonClassNames]}>{buttonTitle}</button>
   );
 };
 
