3465d4a38c6ce3bf9cf78ccdf409e59054567c03
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-07-05 21:48:53 +0300

    feat(UserList): Добавление сортировки

--- src/components/sections/Rankings/UserList/UserList.jsx
+++ src/components/sections/Rankings/UserList/UserList.jsx
@@ -5,45 +5,50 @@ import userListStyles from './UserList.module.scss';
 
 const userListClassName = 'user-list';
 
-const UserList = () => (
-  <div className={userListStyles[userListClassName]}>
-    <div className={userListStyles[`${userListClassName}__list-header`]}>
-      <p className={userListStyles[`${userListClassName}__example-1`]}>#</p>
-      <p className={userListStyles[`${userListClassName}__example-2`]}>Artist</p>
-      <p className={userListStyles[`${userListClassName}__example-3`]}>Change</p>
-      <p className={userListStyles[`${userListClassName}__example-4`]}>NFTs&nbsp;Sold</p>
-      <p className={userListStyles[`${userListClassName}__example-5`]}>Volume</p>
-    </div>
-    {usersData.slice(0, 15).map(({ image, userName, change, nftSold, volume }, userTopIndex) => (
-      <div key={userName} className={userListStyles[`${userListClassName}__creator`]}>
-        <div className={userListStyles[`${userListClassName}__creator-container`]}>
-          <div className={userListStyles[`${userListClassName}__serial-number`]}>{userTopIndex + 1}</div>
-          <div className={userListStyles[`${userListClassName}__artist-details`]}>
-            <div className={userListStyles[`${userListClassName}__avatar`]}>
-              <img alt={image.alt} src={image.src} />
+const UserList = () => {
+  const usersRankings = usersData.slice(0, 15);
+  const sortUsers = usersRankings.sort((a, b) => b.nftSold - a.nftSold);
+
+  return (
+    <div className={userListStyles[userListClassName]}>
+      <div className={userListStyles[`${userListClassName}__list-header`]}>
+        <p className={userListStyles[`${userListClassName}__example-1`]}>#</p>
+        <p className={userListStyles[`${userListClassName}__example-2`]}>Artist</p>
+        <p className={userListStyles[`${userListClassName}__example-3`]}>Change</p>
+        <p className={userListStyles[`${userListClassName}__example-4`]}>NFTs&nbsp;Sold</p>
+        <p className={userListStyles[`${userListClassName}__example-5`]}>Volume</p>
+      </div>
+      {sortUsers.map(({ image, userName, change, nftSold, volume }, userTopIndex) => (
+        <div key={userName} className={userListStyles[`${userListClassName}__creator`]}>
+          <div className={userListStyles[`${userListClassName}__creator-container`]}>
+            <div className={userListStyles[`${userListClassName}__serial-number`]}>{userTopIndex + 1}</div>
+            <div className={userListStyles[`${userListClassName}__artist-details`]}>
+              <div className={userListStyles[`${userListClassName}__avatar`]}>
+                <img alt={image.alt} src={image.src} />
+              </div>
+              <p
+                className={userListStyles[`${userListClassName}__name`]}
+                dangerouslySetInnerHTML={{ __html: userName }}
+              />
             </div>
-            <p
-              className={userListStyles[`${userListClassName}__name`]}
-              dangerouslySetInnerHTML={{ __html: userName }}
+            <div
+              className={userListStyles[`${userListClassName}__change`]}
+              dangerouslySetInnerHTML={{ __html: change }}
+            />
+            <d
+              className={userListStyles[`${userListClassName}__nft-sold`]}
+              dangerouslySetInnerHTML={{ __html: nftSold }}
+            />
+            <div
+              className={userListStyles[`${userListClassName}__volume`]}
+              dangerouslySetInnerHTML={{ __html: volume }}
             />
           </div>
-          <div
-            className={userListStyles[`${userListClassName}__change`]}
-            dangerouslySetInnerHTML={{ __html: change }}
-          />
-          <d
-            className={userListStyles[`${userListClassName}__nft-sold`]}
-            dangerouslySetInnerHTML={{ __html: nftSold }}
-          />
-          <div
-            className={userListStyles[`${userListClassName}__volume`]}
-            dangerouslySetInnerHTML={{ __html: volume }}
-          />
+          <Link to={`/user-profile/${userName}`} />
         </div>
-        <Link to={`/user-profile/${userName}`} />
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export { UserList };
