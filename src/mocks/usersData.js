0d3faa95df85559614299e19dec7b0825c31b2f2
Fedor Krasnov <fedor.o.krasnov@gmail.com>
2024-07-05 21:25:27 +0300

    refactor(userData): Замена статичных данных на динамические

--- src/mocks/usersData.js
+++ src/mocks/usersData.js
@@ -1,163 +1,140 @@
+const numbersGenerate = (minNum = 5, maxNum = 85, decimal = 1) =>
+  (Math.random() * (maxNum - minNum) + minNum).toFixed(decimal);
+
+const randomData = () => ({
+  change: `+${numbersGenerate(1, 9)}%`,
+  nftSold: numbersGenerate(100, 800, 0),
+  volume: `${numbersGenerate()} ETH`,
+});
+
 export const usersData = [
   {
+    ...randomData(),
     image: {
       alt: 'Jaydon Ekstrom Bothman',
       src: '../src/mocks/media/users/user-1.png',
     },
     userName: 'Jaydon Ekstrom Bothman',
-    change: '+1.41%',
-    nftSold: '602',
-    volume: '12.4 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Ruben Carder',
       src: './src/mocks/media/users/user-2.png',
     },
     userName: 'Ruben-Carder',
     name: 'Ruben Carder',
-    change: '+3.56%',
-    nftSold: '789',
-    volume: '15.6 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Alfredo Septimus',
       src: './src/mocks/media/users/user-3.png',
     },
     userName: 'Alfredo Septimus',
-    change: '+5.89%',
-    nftSold: '564',
-    volume: '9.68 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Davis Franci',
       src: './src/mocks/media/users/user-4.png',
     },
     userName: 'Davis Franci',
-    change: '+8.17%',
-    nftSold: '724',
-    volume: '67.81 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Livia Rosser',
       src: './src/mocks/media/users/user-5.png',
     },
     userName: 'Livia Rosser',
-    change: '+8.4%',
-    nftSold: '736',
-    volume: '176.1 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Kianna Donin',
       src: './src/mocks/media/users/user-6.png',
     },
     userName: 'Kianna Donin',
-    change: '+17.614%',
-    nftSold: '266',
-    volume: '17.27 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Phillip Lipshutz',
       src: './src/mocks/media/users/user-7.png',
     },
     userName: 'Phillip Lipshutz',
-    change: '+61.28%',
-    nftSold: '286',
-    volume: '16.83 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Maria Rosser',
       src: './src/mocks/media/users/user-8.png',
     },
     userName: 'Maria Rosser',
-    change: '+71.482%',
-    nftSold: '718',
-    volume: '71.28 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Kianna Stanton',
       src: './src/mocks/media/users/user-9.png',
     },
     userName: 'Kianna Stanton',
-    change: '+91.48%',
-    nftSold: '720',
-    volume: '181.82 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Angel Lubin',
       src: './src/mocks/media/users/user-10.png',
     },
     userName: 'Angel Lubin',
-    change: '+92.47%',
-    nftSold: '671',
-    volume: '18.44 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Allison Torff',
       src: './src/mocks/media/users/user-11.png',
     },
     userName: 'Allison Torff',
-    change: '+92.71%',
-    nftSold: '682',
-    volume: '19.41 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Davis Workman',
       src: './src/mocks/media/users/user-12.png',
     },
     userName: 'Davis Workman',
-    change: '+11.81%',
-    nftSold: '718',
-    volume: '172.92 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Randy Carder',
       src: './src/mocks/media/users/user-13.png',
     },
     userName: 'Randy Carder',
-    change: '+19.482%',
-    nftSold: '722',
-    volume: '65.76 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Lydia Culhane',
       src: './src/mocks/media/users/user-14.png',
     },
     userName: 'Lydia Culhane',
-    change: '+29.454%',
-    nftSold: '545',
-    volume: '145.12 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Rayna Bator',
       src: './src/mocks/media/users/user-15.png',
     },
     userName: 'Rayna Bator',
-    change: '+29.421%',
-    nftSold: '545',
-    volume: '65.239 ETH',
   },
   {
+    ...randomData(),
     image: {
       alt: 'Jocelyn Westervelt',
       src: './src/mocks/media/users/user-9.png',
     },
     userName: 'Jocelyn Westervelt',
-    change: '+29.71%',
-    nftSold: '823',
-    volume: '82.45 ETH',
   },
 ];
